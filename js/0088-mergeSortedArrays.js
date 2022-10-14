567b3b2ca4b33d29f8ad5f2602df2a8c50672e57
Laurelton <name@example.com>
2022-10-13 20:43:19 -0400

    merge sorted arrays

--- js/0088-mergeSortedArrays.js
+++ js/0088-mergeSortedArrays.js
@@ -1,5 +1,5 @@
-//  Time:   
-//  Space:  
+//  Time:   O(N)
+//  Space:  O(1)
 
 /**
  * @param {number[]} nums1
@@ -9,4 +9,24 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
+    let i = m + n - 1;
+    m -= 1;
+    n -= 1;
+    while (m >= 0 && n >= 0) {
+        if (nums2[n] >= nums1[m]) {
+            nums1[i] = nums2[n];
+            n -= 1;
+        } else {
+            nums1[i] = nums1[m];
+            m -= 1;
+        }
+
+        i -= 1;
+    }
+
+    while (n >= 0) {
+        nums1[i] = nums2[n];
+        n -= 1;
+        i -= 1;
+    }
 };
