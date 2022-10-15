b6c54a440005406477a5cff3d65148003c521441
Laurelton <name@example.com>
2022-10-15 12:05:31 -0400

    #922 - sort array by parity - efficient solution

--- js/0922-sortArrayByParityII.js
+++ js/0922-sortArrayByParityII.js
@@ -1,18 +1,21 @@
 //	Time:	O(N) 
-//	Space:	O(N) 
+//	Space:	O(1) 
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var sortArrayByParityII = function(nums) {
-    const evens = nums.filter(n => n % 2 === 0);
-    const odds = nums.filter(n => n % 2 === 1);
-    const output = [];
-    while(odds.length) {
-        output.push(evens.pop(), odds.pop());
+    let odd = 1;
+    for (let even = 0; even < nums.length; even += 2) {
+        if (nums[even] % 2 === 1) {
+            while (nums[odd] % 2 === 1) {
+                odd += 2;
+            }
+            [nums[even], nums[odd]] = [nums[odd], nums[even]]
+        }
     }
 
-    return output;
+    return nums;
 };
 
 const nums = [3,1,4,2];
