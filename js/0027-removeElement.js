ac42808dcf433231a33385f03481a3f920331e2b
Laurelton <name@example.com>
2022-10-15 07:31:07 -0400

    #27 remove element - cleaner solution

--- js/0027-removeElement.js
+++ js/0027-removeElement.js
@@ -7,19 +7,19 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let k = nums.length;
-    let i = 0;
-    let j = nums.length - 1;
-
-    while (i <= j) {
-        if (nums[i] === val) {
-            nums[i] = nums[j];
-            j -= 1;
-            k -= 1;
-        } else {
-            i += 1;
+    let k = 0;  // write index
+    for (let i = 0; i < nums.length; i += 1) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k += 1;
         }
     }
 
     return k;
 };
+
+const nums1 = [0,1,2,2,3,0,4,2];
+const val1 = 2;
+console.log(nums1);
+const k1 = removeElement(nums1, val1);
+console.log(nums1, k1, nums1.slice(0, k1));
