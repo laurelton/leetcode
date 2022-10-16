c2e9b8232d4f07c7cc0d917f2736b8760aa1c30b
Laurelton <name@example.com>
2022-10-16 09:33:49 -0400

    #1051 - height checker - optimal solution Use counting sort to get time to O(n)

--- js/1051-heightChecker.js
+++ js/1051-heightChecker.js
@@ -1,12 +1,28 @@
-//	Time:	O(NlogN)
-//	Space:	O(N)
+//	Time:	O(N)
+//	Space:	O(k) where k is the range of heights
 /**
  * @param {number[]} heights
  * @return {number}
  */
 var heightChecker = function(heights) {
-    return heights
-        .slice()
-        .sort((lo, hi) => lo - hi)
-        .reduce((total, val, indx) => total += (val !== heights[indx]), 0)
+    let result = 0;
+    const heightCounts = new Array(101).fill(0);
+    for (const height of heights) {
+        heightCounts[height] += 1;
+    }
+    
+    // Constraints state the minimum height is 1
+    let currHeight = 1;     
+    for (height of heights) {
+        while(heightCounts[currHeight] === 0) currHeight += 1;
+
+        if (height !== currHeight) result += 1;
+
+        heightCounts[currHeight] -= 1;
+    }
+    return result;
 };
+
+const heights = [5, 4, 3, 2, 1, 5];    //  [1,1,4,2,1,3];
+const count = heightChecker(heights);
+console.log(count);
