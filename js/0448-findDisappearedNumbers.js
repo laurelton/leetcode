ea33ec97c0a0c6d658ba64bb7d80f6396045495a
Laurelton <name@example.com>
2022-10-16 18:22:56 -0400

    #448 - find disappeared numbers - O(n)

--- js/0448-findDisappearedNumbers.js
+++ js/0448-findDisappearedNumbers.js
@@ -1,15 +1,20 @@
-//	Time:	O(N)
-//	Space:	O(N)
+//	Time:	O(N) - two pass
+//	Space:	O(1)
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
     let output = [];
-    const values = new Set(nums);
-    for (let i = 1; i <= nums.length; i++) {
-        if (!values.has(i)) {
-            output.push(i);
+    for (let i = 0; i < nums.length; i += 1) {
+        const abs = Math.abs(nums[i]);
+        const indx = abs - 1;
+        nums[indx] = nums[indx] > 0 ? -nums[indx] : nums[indx];        
+    }
+
+    for (let i = 0; i < nums.length; i += 1) {
+        if (nums[i] > 0) {
+            output.push(i + 1);
         }
     }
 
