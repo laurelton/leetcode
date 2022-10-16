e66d0f9b5e53af488ad3f07193e5b8371ac23c81
Laurelton <name@example.com>
2022-10-16 14:59:43 -0400

    #977 - squares of sorted array - O(N) time

--- js/0977-squaresOfSortedArray.js
+++ js/0977-squaresOfSortedArray.js
@@ -1,13 +1,27 @@
-//	Time:	O(NlogN)
+//	Time:	O(N)
 //	Space:	O(N)
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    return nums
-        .map(n => Math.abs(n) ** 2)
-        .sort((lo, hi) => lo - hi);
+    let l = 0;
+    let r = nums.length - 1;
+    const squares = new Array(nums.length);
+
+    while (l <= r) {
+        const lSq = nums[l] ** 2;
+        const rSq = nums[r] ** 2;
+        if (lSq > rSq) {
+            squares[r - l] = lSq;
+            l += 1;
+        } else {
+            squares[r - l] = rSq;
+            r -= 1;
+        }
+    }
+
+    return squares;
 };
 
 const nums = [-4,-1,0,3,10];
