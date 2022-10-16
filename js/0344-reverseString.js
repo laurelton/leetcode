fdf9e98535693d20eb9c77b0d5b83e497e9e79f4
Laurelton <name@example.com>
2022-10-16 18:29:06 -0400

    #344 - reverse string

---
+++ js/0344-reverseString.js
@@ -0,0 +1,16 @@
+//	Time:	O(n)
+//	Space:	O(1)
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let l = 0;
+    let r = s.length - 1;
+    while (l < r) {
+        [s[l], s[r]] = [s[r], s[l]];
+
+        l += 1;
+        r -= 1;
+    }
+};
