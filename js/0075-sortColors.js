ebf8e1f72a5c183de8e21a3f3ca4329b7a05183e
Laurelton <name@example.com>
2022-10-17 22:18:10 -0400

    #75 - sort colors - 1 pass

--- js/0075-sortColors.js
+++ js/0075-sortColors.js
@@ -1,38 +1,28 @@
-//	Time:	O(n) -> 2 pass
+//	Time:	O(n) -> 1 pass
 //	Space:	O(1)
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    let zeros = 0;
-    let ones = 0;
-    let twos = 0;
-    for (let i = 0; i < nums.length; i += 1) {
+    let l = 0;
+    let r = nums.length - 1;
+    let i = 0;
+    while (i <= r) {
         if (nums[i] === 0) {
-            zeros += 1;
-        } else if (nums[i] === 1) {
-            ones += 1;
+            [nums[i], nums[l]] = [nums[l], nums[i]];
+            l += 1;
+            i += 1;
+        } else if (nums[i] === 2) {
+            [nums[i], nums[r]] = [nums[r], nums[i]];
+            r -= 1;
         } else {
-            twos += 1;
-        }
-    }
-
-    for (let i = 0; i < nums.length; i += 1) {
-        if (zeros > 0) {
-            nums[i] = 0;
-            zeros -= 1;
-        } else if (ones > 0) {
-            nums[i] = 1;
-            ones -= 1;
-        } else {
-            nums[i] = 2;
-            twos -= 1;
+            i += 1;
         }
     }
 };
 
-const nums = [2, 0, 1, 0, 1, 1, 2, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 1];
+const nums = [1,2,0]; //, 0, 1, 1, 2, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 1];
 console.log(nums);
 sortColors(nums);
 console.log(nums);
