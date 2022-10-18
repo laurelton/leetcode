bea3da9077629f2e5bbf9c0e862915e518189909
Laurelton <name@example.com>
2022-10-17 21:29:44 -0400

    #75 - sort colors - 2 pass no swaps

--- js/0075-sortColors.js
+++ js/0075-sortColors.js
@@ -5,24 +5,34 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    let l = 0;
-    let r = nums.length - 1;
+    let zeros = 0;
+    let ones = 0;
+    let twos = 0;
     for (let i = 0; i < nums.length; i += 1) {
         if (nums[i] === 0) {
-            [nums[i], nums[l]] = [nums[l], nums[i]];
-            l += 1;
+            zeros += 1;
+        } else if (nums[i] === 1) {
+            ones += 1;
+        } else {
+            twos += 1;
         }
     }
 
-    for (let i = nums.length - 1; i >= l; i -= 1) {
-        if (nums[i] === 2) {
-            [nums[i], nums[r]] = [nums[r], nums[i]];
-            r -= 1;
+    for (let i = 0; i < nums.length; i += 1) {
+        if (zeros > 0) {
+            nums[i] = 0;
+            zeros -= 1;
+        } else if (ones > 0) {
+            nums[i] = 1;
+            ones -= 1;
+        } else {
+            nums[i] = 2;
+            twos -= 1;
         }
     }
 };
 
-const nums = [2, 0, 1]; //, 0, 1, 1, 2, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 1];
+const nums = [2, 0, 1, 0, 1, 1, 2, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 1];
 console.log(nums);
 sortColors(nums);
 console.log(nums);
