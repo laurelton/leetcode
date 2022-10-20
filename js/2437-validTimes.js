842b84bc1542145e3923d35ca0c1e6f2fc023160
Laurelton <name@example.com>
2022-10-19 21:10:07 -0400

    #2437 - valid times on a clock

--- js/2437-validTimes.js
+++ js/2437-validTimes.js
@@ -1,35 +1,23 @@
-//	Time:	O(1) -> brute force
+//	Time:	O(1) -> regex
 //	Space:	O(1)
 /**
  * @param {string} time
  * @return {number}
  */
 var countTime = function(time) {
-    time = time.replace(/\?/g, '[0-9]');
-    const times = populateTimes();
+    let multiplier = 1;
+    const [hours, minutes] = time.split(':');
+    if (minutes === '??') multiplier *= 60;
+    if (minutes.match(/[0-5]\?/)) multiplier *= 10;
+    if (minutes.match(/\?[0-9]/)) multiplier *= 6;
+    if (hours.match(/2\?/)) multiplier *= 4;
+    if (hours.match(/[0-1]\?/)) multiplier *= 10;
+    if (hours.match(/\?[0-3]/)) multiplier *= 3;
+    if (hours.match(/\?[4-9]/)) multiplier *= 2;
+    if (hours.match(/\?\?/)) multiplier *= 24;
 
-    let count = 0;
-    for (const t of times) {
-        if (t.match(time)) {
-            count += 1;
-        }
-    }
-
-    return count;
+    return multiplier;
 };
-
-var populateTimes = function() {
-    const times = new Set()
-    for (let hour = 0; hour < 24; hour += 1) {
-        for (let min = 0; min < 60; min += 1) {
-            let hourStr = hour.toString().padStart(2, '0');
-            let minStr = min.toString().padStart(2, '0');
-            times.add(`${hourStr}:${minStr}`);
-        }
-    }
-
-    return times;
-}
 
 const testCases = [
     '?5:00',
