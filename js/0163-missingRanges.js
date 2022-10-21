f45eb3b4ea2dcc824adcc981bde0095f25222579
Laurelton <name@example.com>
2022-10-21 10:13:26 -0400

    #163 - missing ranges - more concise

--- js/0163-missingRanges.js
+++ js/0163-missingRanges.js
@@ -8,27 +8,19 @@
  */
 var findMissingRanges = function(nums, lower, upper) {
   const ranges = [];
-  for (const n of nums) {
-    if (n !== lower) {
-        const start = lower;
-        const end = n - 1;
-        const range = getRangeString(start, end);
-        ranges.push(range);
+  const upperNums = nums.concat([upper + 1])
+
+  for (const n of upperNums) {
+    if (n > lower) {
+      const end = n - 1;
+      const range = end === lower ? `${lower}` : `${lower}->${end}`;
+      ranges.push(range);
     }
 
     lower = n + 1;
   }
 
-  if (lower !== upper + 1) {
-    const range = getRangeString(lower, upper);
-    ranges.push(range);
-  }
-
   return ranges;
 };
 
-var getRangeString = function(start, end) {
-    return end - start !== 0 ? `${start}->${end}` : `${start}`;
-}
-
-findMissingRanges([], 0, 99);
+console.log(findMissingRanges([1,3,5,7,9], 0, 10));
