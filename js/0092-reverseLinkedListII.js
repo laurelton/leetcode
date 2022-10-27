ac520302b45d69f1ea55ec4ee70008d96214d6ab
Laurelton <name@example.com>
2022-10-27 11:49:28 -0400

    #92 - reverse linked list II

--- js/0092-reverseLinkedListII.js
+++ js/0092-reverseLinkedListII.js
@@ -1,5 +1,5 @@
-//	Time: 	 O()
-//	Space:	 O()
+//	Time: 	 O(n)
+//	Space:	 O(1)
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -14,43 +14,42 @@
  * @return {ListNode}
  */
 var reverseBetween = function(head, left, right) {
-    let prev = new ListNode(null);
-    let curr = new ListNode(null);
-    curr.next = head;
-    prev.next = curr;
-    let next;
-    let conn;
-    let tail;
-
-    // for (let i = 0; i < left; i += 1) {
-    //     prev = prev.next;
-    //     curr = curr.next;
-    //     next = curr.next;
-    //     con = prev;
-    // }
-    // console.log(`Prev:\t ${prev.val}`);
-    // console.log(`Curr:\t ${curr.val}`);
-    // console.log(`Next:\t ${next.val}`);
-
-    let i = 0;
-    while (i < right) {
-        if (i < left) {
-            prev = prev.next;
-            conn = prev;
-            curr = curr.next;
-            tail = curr;
-            next = curr.next;
-            
-        } else {
-            next = curr.next;
-            curr.next = prev;
-
-            prev = curr;
-            curr = next;
-        }
-        i += 1;
+    // Create sentinel node to allow reversing from head
+    // const sntl = new ListNode(0, head);
+    const sntl = {next: head};
+    let curr = sntl;
+    
+    // Iterate to node left - 1
+    for (let i = 1; i < left; i += 1) {
+        curr = curr.next;
     }
-    conn.next = curr;
-    tail.next = next;
 
+    // Set left - 1 as tail of left portion of list
+    const leftTail = curr;
+    // set left as tail of reversed sublist
+    const revTail = curr.next;
+
+    // Create pointer to left, and left + 1
+    // reverse sublist to right
+    let prev = null;
+    let next = null;
+    curr = curr.next;
+    for (i = left; i <= right; i += 1) {
+        next = curr.next;
+        curr.next = prev;
+
+        [prev, curr] = [curr, next];
+    }
+
+    // set right as head of reversed sublist
+    // Set right + 1 as head of right portion of list
+    const revHead = prev;
+    const rightHead = next;
+
+    // Attach head of sublist to tail of left
+    // Attach tail of sublist to head of right
+    leftTail.next = revHead;
+    revTail.next = rightHead;
+
+    return sntl.next;
 };
