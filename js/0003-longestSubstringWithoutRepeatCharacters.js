f740f26feb45a09c291fc7a28295b59a29efd9a5
Laurelton <name@example.com>
2022-11-26 20:24:15 -0500

    #3 - longest substring without repeat chars

--- js/0003-longestSubstringWithoutRepeatCharacters.js
+++ js/0003-longestSubstringWithoutRepeatCharacters.js
@@ -1,24 +1,25 @@
 //	Time: 	 O(n)
-//	Space:	 O(m) - m is the size of the characte set
+//	Space:	 O(m) - m is the size of the character set
 /**
- * @param {string} s
+ * @param {string} str
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    const counts = new Map();
-    let mx = 0;
+var lengthOfLongestSubstring = function(str) {
+    const count = new Map();
+    let longest = 0;
     let l = 0;
-    let r = 0;
-    while (r < s.length) {
-        if (~~counts.get(s[r]) > 0) {
-            while (counts.get(s[r])) {
-                counts.delete(s[l++]);
-            }
+    for (let r = 0; r < str.length; r += 1) {
+        const curr = str[r];
+        while (~~count.get(curr)) {
+            const left = str[l++];
+            count.set(left, count.get(left) - 1);
         }
-        mx = Math.max(mx, r - l + 1);
-        counts.set(s[r], 1);
-        r += 1;
+
+        count.set(curr, ~~count.get(curr) + 1);
+        longest = Math.max(r - l + 1, longest);
     }
-    
-    return mx;
+
+    return longest;
 };
+
+console.log(lengthOfLongestSubstring('pwwkew'));
